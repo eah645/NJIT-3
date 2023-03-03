62bc596f26ed9f6367da99e1cd580592e59ec17e
eah645 <92736056+eah645@users.noreply.github.com>
2023-03-03 10:55:56 -0500

    Milestone 4

--- site.js
+++ site.js
@@ -21,28 +21,83 @@
 */
 
 const vue_app = Vue.createApp({
-      // This automatically imports your movies.json file and puts it into
-      //   the variable: movies
-      created () {
-            fetch('movies.json').then(response => response.json()).then(json => {
-                  this.movies = json
-            })
-      },
-      data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-          title:  "IMDB + Erin's Top 8 Movies",
-          owner: "Erin",
-          github: "https://github.com/eah645/NJIT-3.git"
+  // This automatically imports your movies.json file and puts it into
+  //   the variable: movies
+  created() {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((json) => {
+        this.movies = json;
+      });
+  },
+  data() {
+    return {
+      // This holds your movies.json data.
+      movies: [],
+      /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+      title: "IMDB + Erin's Top 8 Movies",
+      owner: "Erin",
+      github: "https://github.com/eah645/NJIT-3.git",
+    };
+  },
+  methods: {
+    /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+    getMonthText(dateArray) {
+      let month = "";
+      switch (dateArray[1]) {
+        case 1:
+          month = "January";
+          break;
+        case 2:
+          month = "February";
+          break;
+        case 3:
+          month = "March";
+          break;
+        case 4:
+          month = "April";
+          break;
+        case 5:
+          month = "May";
+          break;
+        case 6:
+          month = "June";
+          break;
+        case 7:
+          month = "July";
+          break;
+        case 8:
+          month = "August";
+          break;
+        case 9:
+          month = "September";
+          break;
+        case 10:
+          month = "October";
+          break;
+        case 11:
+          month = "November";
+          break;
+        case 12:
+          month = "December";
+      }
+      return month + " " + dateArray[2] + ", " + dateArray[0];
+    },
+    posterClick(movie) {
+      movie.posterindex++;
+      if (movie.posterindex > movie.posters.length - 1) {
+        movie.posterindex = 0;
       }
     },
-      methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-      }
-})
+    timeText(minutes) {
+      //https://codingbeautydev.com/blog/javascript-convert-minutes-to-hours-and-minutes/
+      var hours = Math.floor(minutes / 60);
+      var minutes = minutes % 60;
 
-
+      return hours + ' hours, ' + minutes + ' minutes';
+    }
+    }
+  }
+);
 
 vue_app.mount("#vue_app")
